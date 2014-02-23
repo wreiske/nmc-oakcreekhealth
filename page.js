b07d49eafdfbe88e1599fbbb565bfa5cc817afa2
William Reiske <wreiske@mieweb.com>
2014-02-22 20:31:27 -0500

    Init commit for NMC Oak Creek Health

---
+++ page.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $('.demo').click(function(e) {
+        e.preventDefault();
+        alert('Function disabled in demo.');
+    });
+
+
+    window.addEventListener("orientationchange", updateOrientation);
+
+});
+
+
+function updateOrientation(e) {
+    switch (e.orientation) {
+        case 0:
+            // Do your thing
+            break;
+
+        case -90:
+            // Do your thing
+            break;
+
+        case 90:
+            // Do your thing
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+function scan() {
+    cordova.plugins.barcodeScanner.scan(
+        function(result) {
+            /* 
+            alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+			*/
+			window.location='http://64.186.38.68:3010/ccda';
+        },
+        function(error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+}
