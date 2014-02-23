999fdcc87f0afd72c21f73075dd24d2089e968cf
William Reiske <wreiske@mieweb.com>
2014-02-23 17:55:14 -0500

    Removed hard links

--- convert.py
+++ convert.py
@@ -6,20 +6,20 @@ from progressbar import ProgressBar, Percentage, Bar
 
 print "------------- Will's Image Converter --------------"
 
-input_file = '/Users/wreiske/nmc/nmc/www/icon.png'
+input_file = './icon.png'
 
 files = {
-	'/Users/wreiske/nmc/nmc/www/res/icon/android/icon-96-xhdpi.png':'96x96',
-	'/Users/wreiske/nmc/nmc/www/res/icon/android/icon-72-hdpi.png':'72x72',
-	'/Users/wreiske/nmc/nmc/www/res/icon/android/icon-48-mdpi.png':'48x48',
-	'/Users/wreiske/nmc/nmc/www/res/icon/android/icon-36-ldpi.png':'36x36',
-	'/Users/wreiske/nmc/nmc/www/res/icon/ios/icon-72-2x.png':'144x144',
-	'/Users/wreiske/nmc/nmc/www/res/icon/ios/icon-57-2x.png':'114x114',
-	'/Users/wreiske/nmc/nmc/www/res/icon/ios/icon-57.png':'57x57',
-	'/Users/wreiske/nmc/nmc/www/res/icon/ios/icon-72.png':'72x72',
-	'/Users/wreiske/nmc/nmc/www/res/icon/windows-phone/icon-173-tile.png':'173x173', 
-	'/Users/wreiske/nmc/nmc/www/res/icon/windows-phone/icon-62-tile.png':'62x62',
-	'/Users/wreiske/nmc/nmc/www/res/icon/windows-phone/icon-48.png':'48x48'
+	'./res/icon/android/icon-96-xhdpi.png':'96x96',
+	'./res/icon/android/icon-72-hdpi.png':'72x72',
+	'./res/icon/android/icon-48-mdpi.png':'48x48',
+	'./res/icon/android/icon-36-ldpi.png':'36x36',
+	'./res/icon/ios/icon-72-2x.png':'144x144',
+	'./res/icon/ios/icon-57-2x.png':'114x114',
+	'./res/icon/ios/icon-57.png':'57x57',
+	'./res/icon/ios/icon-72.png':'72x72',
+	'./res/icon/windows-phone/icon-173-tile.png':'173x173', 
+	'./res/icon/windows-phone/icon-62-tile.png':'62x62',
+	'./res/icon/windows-phone/icon-48.png':'48x48'
 }
 
 file_count = len(files)
